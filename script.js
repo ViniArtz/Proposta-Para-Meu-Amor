ba879c4d21987eab7a30fc7b024ab92886a56b03
Paulo Vinicius <viniciuszm15@gmail.com>
2024-05-12 11:37:20 -0300

    comiit

---
+++ script.js
@@ -0,0 +1,14 @@
+function SIM (){
+    alert("Te amarei Para Todo o Sempre Minha Princesa <3")
+}
+
+
+
+function moveButton(x) {
+    marginX = Math.floor(Math.random()*600)
+    marginY = Math.floor(Math.random()*600)
+
+    x.style.margin = marginY.toString() + 'px ' + marginX.toString() + 'px'
+}
+
+    
